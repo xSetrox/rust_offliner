d8e615e332e2119569d1367e68d71bffd4851e60
Setro <lance.faltinsky@gmail.com>
2021-06-20 21:35:42 -0400

    added readme and support for multiple players

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from time import sleep
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-
-name = input('Input exact player name of who you would like to offline raid: ')
+names = config['rust_offliner']['playernames'].split(',')
 ip, port = config['rust_offliner']['ip'].split(':')
 addr = (ip, int(port))
 while True:
     plys = [p.name for p in a2s.players(addr)]
     print('Current players: ', ', '.join(plys))
-    if name not in plys:
-        print(name, "does not appear to be on the server! Now is the time to offline.")
-        ctypes.windll.user32.MessageBoxW(0, f'Player {name} was not found on the server. Time to offline.', 'Rust offliner: It is time', 0x1000)
+    for name in names:
+        if name not in plys:
+            print(f'\"{name}\" does not appear to be on the server!')
+            print(f'Monitoring is now OFF for \"{name}\" as they have went offline.')
+            names.remove(name)
+            ctypes.windll.user32.MessageBoxW(0, f'Player \"{name}\" was not found on the server. Time to offline.', 'Rust offliner: It is time', 0x1000)
+    if not names:
+        print('No more names left to monitor. Exiting.')
         break
     sleep(10)
---
+++ readme.md
@@ -0,0 +1,17 @@
+### This program is for educational purposes only. It's not made to make Rust any more toxic.
+
+# rust_offliner
+Monitors a Rust gameserver and alerts you when a player disconnects. Can also monitor multiple playernames at once.
+
+## How it works
+Rust uses Source as its server system. Because it does, we are able to query the server using A2S queries and get info on the server,
+such as amount of players, map name, etc. This is the reason you are able to view servers through Steam without actually being in the game.
+
+## Setup
+Set the server IP:port in config.ini.  
+Then set the names of the players to monitor in config.ini as well, separated by commas.  
+Done.
+## Fake players
+Apparently a whole lot of servers use fake players to boost their player count, so you will see them whenever the program prints the player list (they have basic names like christopher, etc). This will not affect your player monitoring as long as you do not enter the name of a fake player.
+
+
